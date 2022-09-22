c282b37a1d2c4af9f6895fb9edd6b90b399f9c59
serhdheeq <serhdheeq@gmail.com>
2022-09-22 12:37:43 -0700

    task 0-7

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strlen - function that return the lenght of a string
+ * @s: the string
+ * Return: length of s
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i))
+		i++;
+
+	return (i);
+}
+
+/**
+ * _strncat - function that concatenates two strings
+ * @dest: first string
+ * @src: second string
+ * @n: bytes from src the function will use
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, ld;
+
+	ld = _strlen(dest);
+
+	for (i = ld, j = 0; *(src + j) && j < n; i++, j++)
+	{
+		*(dest + i) = *(src + j);
+	}
+	*(dest + i) = '\0';
+
+	return (dest);
+}
